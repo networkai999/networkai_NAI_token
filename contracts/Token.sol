aa32dd80d42b2fdabd90cf5d667dd6ae77c297ef
networkai999 <riccardovazzoler@outlook.it>
2023-05-01 13:30:09 +0200

    update transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,45 +12,62 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes  {
    
     address _primaryOwner;
+    uint256 _mintAmount = 100000000 * 10**18;
     uint256 supplementaryTkRicompense;
-    
+    uint256 maxSupply = 100000000 * 10**18;
+    uint256 ownerAmount =  _mintAmount / 10;
+
+
     mapping (address => uint256) public _balances;
-    event Transfer(address _owner, uint256 value);
+    event TransferSent(address _from,address _to, uint256 _amount);
+    event TransferMint(address _owner,uint256 _amount);
     
     
     constructor() ERC20("NetworkAi", "NAI") ERC20Permit("NetworkAi") {
-        _mint(msg.sender, 50000000 * 10**18);
         _primaryOwner = msg.sender;
+        _mint(_primaryOwner, _mintAmount);
+        _balances[_primaryOwner] = ownerAmount;
+        emit TransferMint(_primaryOwner, ownerAmount);
 
-        emit Transfer(msg.sender, 50000000 * 10**18);
     }
 
 
     function transfer(address _receiver,uint256 _amount) public override returns (bool) {
 
 
-        require(_balances[msg.sender] >= _amount , "Insufficient balance");
+        require(_balances[msg.sender] >= _amount, "Insufficient balance");
         require(_receiver != msg.sender , "You cannot transfer to yourself");
         
+        _beforeTokenTransfer(msg.sender, _receiver, _amount);
+
         _balances[msg.sender] = _balances[msg.sender] - _amount;
         _balances[_receiver] = _balances[_receiver] + _amount;
 
-        emit Transfer(_receiver, _amount);
-
+        emit TransferSent(msg.sender, _receiver, _amount);
+        _afterTokenTransfer(msg.sender, _receiver, _amount);
+        emit Transfer(msg.sender, _receiver, _amount);
+        
         return true;
+       
+    }
+
+    event Received(address, uint);
+        receive() external payable {
+        emit Received(msg.sender, msg.value);
     }
 
 
 
-    function Ricompense(address _to) public  {
+    function Ricompense(address _receiver) public  {
         require(msg.sender == _primaryOwner , "You aren't owner");
         require(supplementaryTkRicompense <= (50000000 * 10**18) - (10 * 10**18) , 
             "Maximum rewards achieved"
         );
-        require(_to != msg.sender , "You cannot transfer to yourself");
+        require(_receiver != msg.sender , "You cannot transfer to yourself");
 
-        _mint(_to, 10 * 10**18);
+        _mint(_receiver, 10 * 10**18);
         supplementaryTkRicompense += 10 * 10**18;
+        emit TransferSent(msg.sender, _receiver, 10 * 10**18);
        
     }
 
