be3a3d0a34109d94e83e52b7c25c1301792e3b4a
networkai999 <riccardovazzoler@outlook.it>
2023-05-02 09:42:28 +0200

    more methods avaiable

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,29 +5,50 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 
 
 
-contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes  {
+contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes , ERC20Capped  {
    
-    address _primaryOwner;
-    uint256 _mintAmount = 100000000 * 10**18;
+    // address owner
+    address primaryOwner;
+    // initial mint amount
+    uint256 mintAmount = 100000000 * 10**18;
+    // number of reward tokens issued
     uint256 supplementaryTkRicompense;
-    uint256 maxSupply = 100000000 * 10**18;
-    uint256 ownerAmount =  _mintAmount / 10;
+    // max supply
+    uint256 maxSupply = 150000000 * 10**18;
+    // tokens for tasks completed by users
+    uint256 ricompense = 10 * 10**18;
+    // Check whether the user can claim his or her tokens 
+    uint256 private _tokenChecker = 10278359485746330984340085;
+    // Counter that calculates how many times tokens have been claimed
+    int claimedCount = 0;
+    // tokens released from rewards
+    int tokenReleasedFR = 0;
+    // max ricompense for claim
+    uint256 maxRicompense = 50000000 * 10**18;
+    
 
 
     mapping (address => uint256) public _balances;
     event TransferSent(address _from,address _to, uint256 _amount);
     event TransferMint(address _owner,uint256 _amount);
+    event Received(address, uint);
+        receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
     
     
-    constructor() ERC20("NetworkAi", "NAI") ERC20Permit("NetworkAi") {
-        _primaryOwner = msg.sender;
-        _mint(_primaryOwner, _mintAmount);
-        _balances[_primaryOwner] = _mintAmount;
-        emit TransferMint(_primaryOwner, _mintAmount);
+    constructor() ERC20("NetworkAi", "NAI") ERC20Permit("NetworkAi") ERC20Capped(maxSupply) {
+        primaryOwner = msg.sender;
+        _mint(primaryOwner, mintAmount);
+        _balances[primaryOwner] = mintAmount;
+
+        emit TransferMint(primaryOwner, mintAmount);
 
     }
 
@@ -39,24 +60,26 @@ contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes  {
         
     }
 
-    event Received(address, uint);
-        receive() external payable {
-        emit Received(msg.sender, msg.value);
-    }
 
-
-
-    function Ricompense(address _receiver) public  {
-        require(msg.sender == _primaryOwner , "You aren't owner");
-        require(supplementaryTkRicompense <= (50000000 * 10**18) - (10 * 10**18) , 
+    function getClaim(address _receiver,uint256 _receiverToken) public  {
+        
+        require(_receiverToken == _tokenChecker, "You can't get your tokens");
+        require(supplementaryTkRicompense <= (maxRicompense - ricompense) , 
             "Maximum rewards achieved"
         );
-        require(_receiver != msg.sender , "You cannot transfer to yourself");
 
-        _mint(_receiver, 10 * 10**18);
-        supplementaryTkRicompense += 10 * 10**18;
-        emit TransferSent(msg.sender, _receiver, 10 * 10**18);
+        NetworkAi._mint(_receiver, ricompense);
+        _balances[_receiver] = ricompense;
        
+        supplementaryTkRicompense += ricompense;
+        emit TransferMint(_receiver, ricompense);
+
+        // ++counter rewards
+        claimedCount++;
+
+        // + tokenReleasedFR
+        tokenReleasedFR +=  10;
+
     }
 
 
@@ -64,7 +87,26 @@ contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes  {
     function balanceOf() public view returns (uint256) {
         return _balances[msg.sender];
     }
+
+    // Function that returns the count of reward requests
+    function countRewards() public view returns (int) {
+        return claimedCount;
+    }
+
+    // Function that returns the number of tokens released so far from the rewards
+    function tokenRFR() public view returns (int) {
+        return tokenReleasedFR;
+    }
     
+    // returns max supply
+    function getMaxSupply() public view returns (uint256) {
+        return maxSupply;
+    }
+
+    // return supply in this moment
+    function getSupply() public view returns (uint256) {
+        return totalSupply();
+    }
 
     function _afterTokenTransfer(address from, address to, uint256 amount)
         internal
@@ -76,7 +118,7 @@ contract NetworkAi is ERC20,Ownable , ERC20Permit, ERC20Votes  {
 
     function _mint(address to, uint256 amount)
         internal
-        override(ERC20, ERC20Votes)
+        override(ERC20, ERC20Votes , ERC20Capped)
     {
         super._mint(to, amount);
     }
